8536c1d4f929a46d1f3ac390f4c9f1534067fe2d
T0a5t-E <gerardogongora.personal@gmail.com>
2025-01-10 08:57:45 -0600

    Creando el metodo atacar

--- poo_con_python.py
+++ poo_con_python.py
@@ -1,15 +1,39 @@
 class Personaje:
-    # Attributes
-    nombre = 'default'
-    fuerza = 0
-    inteligencia = 0
-    defensa = 0
-    vida = 0
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
 
-# Constructor variable of the class
-mi_personaje = Personaje()
+    def atributos(self):
+        print(f"Nombre: {self.nombre}\nFuerza: {self.fuerza}\nInteligencia: {self.inteligencia}\nDefensa: {self.defensa}\nVida: {self.vida}")
+    
+    def subir_nivel(self, fuerza, inteligencia, defensa, vida):
+        self.fuerza += fuerza
+        self.inteligencia += inteligencia
+        self.defensa += defensa
+        self.vida += vida
 
-mi_personaje.nombre = "queso"
-mi_personaje.fuerza = 666
-print("El nombre del personaje es", mi_personaje.nombre)
-print("La fuerza del personaje es", mi_personaje.fuerza)
+    def confirmar_vida(self):
+        return self.vida > 0
+    
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+    
+    def dmg(self, enemigo):
+        return enemigo.defensa - self.fuerza
+
+    def atacar(self, enemigo):
+        dmg = self.dmg(enemigo)
+        enemigo.vida -= dmg
+        print(self.nombre, "ha hecho", dmg, "de daño a", enemigo.nombre)
+        print("La vida de", enemigo.nombre, "es", enemigo.vida)
+
+mi_personaje = Personaje("Abadon", 616, 616, 616, 616) # Character constructor
+mi_enemigo = Personaje("Gabriel", 777, 777, 777, 777)
+print()
+mi_personaje.atributos()
+mi_personaje.atacar(mi_enemigo)
+mi_enemigo.atributos()
