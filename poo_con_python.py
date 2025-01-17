302e73a630ce218fcbdeefce37e8b11240e78c8e
T0a5t-E <gerardogongora.personal@gmail.com>
2025-01-17 08:49:06 -0600

    Problema Celestial

--- poo_con_python.py
+++ poo_con_python.py
@@ -8,6 +8,7 @@ class Personaje:
 
     def atributos(self):
         print(f"Nombre: {self.__nombre}\nFuerza: {self.__fuerza}\nInteligencia: {self.__inteligencia}\nDefensa: {self.__defensa}\nVida: {self.__vida}")
+        print(hercules.espada)
     
     def subir_nivel(self, fuerza, inteligencia, defensa, vida):
         self.__fuerza += fuerza
@@ -23,7 +24,7 @@ class Personaje:
         print(self.__nombre, "ha muerto")
     
     def dmg(self, enemigo):
-        return enemigo.__defensa - self.__fuerza
+        return self.__fuerza * self.espada - enemigo.__defensa
 
     def atacar(self, enemigo):
         dmg = self.dmg(enemigo)
@@ -39,10 +40,36 @@ class Personaje:
         self.__fuerza = fuerza if fuerza > 0 else print("La fuerza debe ser mayor a 0")
     
 class Guerrero(Personaje):
-    # Sobreescribir constructor
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
         super().__init__(nombre, fuerza, inteligencia, defensa, vida)
         self.espada = espada
 
-hercules = Guerrero("Hercules", 100, 100, 100, 100)
+    def cambiar_arma(self):
+        opcion = int(input(f"Elige un arma:\n(1) Espada de plata, DMG = 10\n(2) Espada de bronce, DMG = 8\n>> "))
+        if opcion == 1:
+            self.espada = 10
+        elif opcion == 2:
+            self.espada = 8
+        else:
+            self.espada = 5
+
+class Mago(Personaje):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.libro = libro
+    
+    def atributos(self):
+        return super().atributos()
+        print("- Libro:", self.libro)
+
+    def dmg(self, enemigo):
+        return self.__inteligencia * self.libro - enemigo.__defensa
+
+hercules = Guerrero("Hercules", 80, 50, 100, 100, 10)
+abramelin = Mago("Abramelin", 33, 77, 77, 77, 77)
 hercules.atributos()
+abramelin.atributos()
+abramelin.atacar(hercules)
+hercules.atacar(abramelin)
+hercules.atributos()
+abramelin.atributos()
